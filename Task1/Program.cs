9d58de72025a51affdba5ad23313ca536a6950ca
Aleksandr <maivan007@yandex.ru>
2022-07-02 13:20:00 +0400

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿void Zadacha13(string numb)
+{
+    if(numb.Length > 2)
+    {
+        Console.WriteLine(numb[2]);
+    }
+    else
+    {
+        Console.WriteLine("В числе третьей цифры нет");
+    }
+}
+
+Console.Write("Введите число:");
+string number = Console.ReadLine();
+Zadacha13(number);
