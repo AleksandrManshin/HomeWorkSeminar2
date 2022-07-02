1630fb664b702a4011b414dd11cd8ddd70008091
Aleksandr <maivan007@yandex.ru>
2022-07-02 13:07:53 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿void Zadacha15(int numb)
+{
+    if(numb == 6)
+    {
+        Console.WriteLine("Да");
+    }
+    else if(numb == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else if(numb < 1)
+    {
+        Console.WriteLine("Такого дня недели нет");
+    }
+    else if(numb > 7)
+    {
+        Console.WriteLine("Такого дня недели нет");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}   
+
+Console.WriteLine("Введите цифру дня недели, а я скажу является ли этот день выходным:");
+int number = Convert.ToInt32(Console.ReadLine());
+Zadacha15(number);
